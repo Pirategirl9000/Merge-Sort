62d1b698905527036f81c35d959657f3ffd2280b
Violet <136397110+Pirategirl9000@users.noreply.github.com>
2024-03-12 20:21:39 +0000

    Getting close I think

--- script.js
+++ script.js
@@ -1,22 +1,41 @@
 let array = [-8, 6, 0, 9, -4, 16];
 
 function merge(array, p, q, r) {
-    let k = 0;
-    let i = 0;
-    let j = 0;
+
+
+    let k = p;
+    let i;
+    let j;
+
     let lowerHalf = [];
     let upperHalf = [];
 
-    for (let n = 0; n < q; n++) {
-        lowerHalf[n] = array[n];
+    for (i = 0; k <= q; i++, k++) {
+        lowerHalf[i] = array[k];
+    }
+    for (j = 0; k <= r; j++, k++) { //needs j to = 0 so it fills in second array starting at index [0] instead of skipping forward
+        upperHalf[j] = array[k];
     }
 
-    for (let n = q + 1; n < r; n++) {
-        upperHalf[n] = array[n];
+    k = p;
+    i = 0;
+    j = 0;
+
+    for (let k = 0; k <= r; k++) {
+        if (i > array.length-1) {array[k] = upperHalf[j]; break;}
+        if (j > array.length-1) {array[k] = lowerHalf[i]; break;}
+
+        if (lowerHalf[i] < upperHalf[j]) {
+            array[k] = lowerHalf[i];
+            i++;
+        } else {
+            array[k] = upperHalf[j];
+            j++
+        }
     }
 
-    console.log(lowerHalf);
-    console.log(upperHalf);
+
+
 }
 
 function mergeSort(array, p, r) {
